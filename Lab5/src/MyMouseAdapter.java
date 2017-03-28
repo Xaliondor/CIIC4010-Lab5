f79ad384b9b98088126be05108a3df1ddb279f8b
Xaliondor <reptiliosragnos@hotmail.com>
2017-03-27 22:34:02 -0400

    aa

--- Lab5/src/MyMouseAdapter.java
+++ Lab5/src/MyMouseAdapter.java
@@ -76,52 +76,32 @@ public class MyMouseAdapter extends MouseAdapter {
                         //Do nothing
                     } else {
                         //Released the mouse button on the same cell where it was pressed
-                        if ((gridX == 0)) {
-                        	//Released on left column
-                        	
-                        	Color newColor = null;
-                        	Color oldColor = myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY];
-
-
-                        	int col = 0;
-                        	while (col <= 9){
-                        		
-                        		col = col + 1;
-                        		
-                        		int i = generator.nextInt(5);
-
-                            	switch (i) {
-                            	case 0:
-                            		newColor = Color.YELLOW;
-                            		break;
-                            	case 1:
-                            		newColor = Color.MAGENTA;
-                            		break;
-                            	case 2:
-                            		newColor = Color.BLACK;
-                            		break;
-                            	case 3:
-                            		newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-                            		break;
-                            	case 4:
-                            		newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-                            		break;
-                            	}
-                        		
-                        		myPanel.colorArray[myPanel.mouseDownGridX+col][myPanel.mouseDownGridY] = newColor;
-                        	}
-
-                        	myPanel.repaint();
-                        	
-                        } else if (gridY == 0) {
-                        	//Released on top row
-                        	
-                        } else {
-                            //On the grid other than on the left column and on the top row:
-                            Color newColor = null;
-                            Color oldColor = myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY];
+                    	Color newColor = null;
+                        Color oldColor = myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY];
+                       
+                        int i = generator.nextInt(5);
+                       
+                        switch (i) {
+                        case 0:
+                            newColor = Color.YELLOW;
+                            break;
+                        case 1:
+                            newColor = Color.MAGENTA;
+                            break;
+                        case 2:
+                            newColor = Color.BLACK;
+                            break;
+                        case 3:
+                            newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
+                            break;
+                        case 4:
+                            newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
+                            break;
+                        }
+                       
+                        while (newColor.equals(oldColor)) {
                            
-                            int i = generator.nextInt(5);
+                            i = generator.nextInt(5);
                            
                             switch (i) {
                             case 0:
@@ -140,32 +120,10 @@ public class MyMouseAdapter extends MouseAdapter {
                                 newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
                                 break;
                             }
-                           
-                            while (newColor.equals(oldColor)) {
-                               
-                                i = generator.nextInt(5);
-                               
-                                switch (i) {
-                                case 0:
-                                    newColor = Color.YELLOW;
-                                    break;
-                                case 1:
-                                    newColor = Color.MAGENTA;
-                                    break;
-                                case 2:
-                                    newColor = Color.BLACK;
-                                    break;
-                                case 3:
-                                    newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-                                    break;
-                                case 4:
-                                    newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-                                    break;
-                                }
-                            }
-                            myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
-                            myPanel.repaint();
                         }
+                        myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
+                        myPanel.repaint();
+                            
                     }
                 }
             }
--- Lab5/src/MyPanel.java
+++ Lab5/src/MyPanel.java
@@ -10,10 +10,10 @@ public class MyPanel extends JPanel {
 	private static final int GRID_X = 25;
 	private static final int GRID_Y = 25;
 	private static final int INNER_CELL_SIZE = 29;
-	private static final int TOTAL_COLUMNS = 10;
-	private static final int TOTAL_ROWS = 11;   //Last row has only one cell
-	public int x = -1;
+	private static final int TOTAL_COLUMNS = 9;
+	private static final int TOTAL_ROWS = 9;  
 	public int y = -1;
+	public int x = -1;
 	public int mouseDownGridX = 0;
 	public int mouseDownGridY = 0;
 	public Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
@@ -27,16 +27,9 @@ public class MyPanel extends JPanel {
 		if (TOTAL_ROWS + (new Random()).nextInt(1) < 3) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("TOTAL_ROWS must be at least 3!");
 		}
-		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Top row
-			colorArray[x][0] = Color.LIGHT_GRAY;
-		}
-		for (int y = 0; y < TOTAL_ROWS; y++) {   //Left column
-			colorArray[0][y] = Color.LIGHT_GRAY;
-		}
-		for (int x = 1; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
-			for (int y = 1; y < TOTAL_ROWS; y++) {
-				colorArray[x][y] = Color.WHITE;
-			}
+		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
+			for (int y = 0; y < TOTAL_ROWS; y++) {
+				colorArray[x][y] = Color.LIGHT_GRAY;			}
 		}
 	}
 	public void paintComponent(Graphics g) {
@@ -52,26 +45,22 @@ public class MyPanel extends JPanel {
 		int height = y2 - y1;
 
 		//Paint the background
-		g.setColor(Color.LIGHT_GRAY);
+		g.setColor(Color.WHITE);
 		g.fillRect(x1, y1, width + 1, height + 1);
 
-		//Draw the grid minus the bottom row (which has only one cell)
-		//By default, the grid will be 10x10 (see above: TOTAL_COLUMNS and TOTAL_ROWS) 
+		//Draws the Grid
 		g.setColor(Color.BLACK);
-		for (int y = 0; y <= TOTAL_ROWS - 1; y++) {
+		for (int y = 0; y <= TOTAL_ROWS; y++) {
 			g.drawLine(x1 + GRID_X, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X + ((INNER_CELL_SIZE + 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
 		}
 		for (int x = 0; x <= TOTAL_COLUMNS; x++) {
-			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)));
+			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS)));
 		}
-
-		//Draw an additional cell at the bottom left
-		g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1);
 
 		//Paint cell colors
 		for (int x = 0; x < TOTAL_COLUMNS; x++) {
 			for (int y = 0; y < TOTAL_ROWS; y++) {
-				if ((x == 0) || (y != TOTAL_ROWS - 1)) {
+				if ((x == 0) || (y != TOTAL_ROWS)) {
 					Color c = colorArray[x][y];
 					g.setColor(c);
 					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
@@ -96,10 +85,10 @@ public class MyPanel extends JPanel {
 		}
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
-		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
+		if (x == 0 && y == TOTAL_ROWS) {    //The lower left extra cell
 			return x;
 		}
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 1) {   //Outside the rest of the grid
 			return -1;
 		}
 		return x;
@@ -121,10 +110,10 @@ public class MyPanel extends JPanel {
 		}
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
-		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
+		if (x == 0 && y == TOTAL_ROWS) {    //The lower left extra cell
 			return y;
 		}
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 1) {   //Outside the rest of the grid
 			return -1;
 		}
 		return y;
